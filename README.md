26cf1e828c4a47ee36e26a1eaa76cd9c7a6458bd
Philippe Bertrand <bertrand.philippe22@gmail.com>
2023-02-16 11:36:47 -0500

    GPS Button and GPSActivity added

--- README.md
+++
@@ -1,9 +0,0 @@
-# ENGR-390_Group2
-
-Student Names:
- <br>Philippe Bertrand - 4015740
- <br>Alexandre Fontaine - 40158602
- <br>Elyes Bahri - 40124546
- <br>Sayema Master - 40135481
- <br>Huiling Chen - 40171114
- <br>Gajanan Niranjanan - 40188296
--- UICode/app/src/main/java/com/example/ui_code/MainActivity.java
+++ UICode/app/src/main/java/com/example/ui_code/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.ui_code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonGPS;
+
+    private Button buttonHR;
+
+    private Button buttonAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //generate visuals
+        setupUI();
+
+        onClickListeners();
     }
+
+    private void onClickListeners() {
+        buttonGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoGPSActivity();
+            }
+        });
+    }
+
+    private void gotoGPSActivity() {
+        Intent intent = new Intent(this, GPSActivity.class);
+        startActivity(intent);
+    }
+
+    private void setupUI() {
+        buttonGPS = findViewById(R.id.gpsButtonId);
+        buttonHR = findViewById(R.id.heartrateButtonId);
+        buttonAcc = findViewById(R.id.accelerometerButtonId);
+    }
+
+
 }
