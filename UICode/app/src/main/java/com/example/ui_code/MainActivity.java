14102ffaa71033f7477dd96a8ca30731eb2ef901
philippebertrand22 <90271955+philippebertrand22@users.noreply.github.com>
2023-03-04 17:55:34 -0500

    implemented timer and play/pause/stop button
    timer runs within the app and the buttons can changed later if required

--- UICode/app/src/main/java/com/example/ui_code/MainActivity.java
+++ UICode/app/src/main/java/com/example/ui_code/MainActivity.java
@@ -5,31 +5,106 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button summaryButton;
+    private ImageView stopButton, playButton, pauseButton;
+
+    private TextView timeValue, timeText, distanceValue, distanceText, speedValue, speedText, BPMValue, BPMText;
+
+    boolean timerStarted = false;
+
+    Timer timer;
+    TimerTask timerTask;
+    Double time = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        timer = new Timer();
         setupUI();
-
         onClickListeners();
+        stopButton.setVisibility(View.GONE);
+        playButton.setVisibility(View.GONE);
+        startTimer();
+    }
+
+    private void startTimer() {
+            if(timerStarted == false){
+                timerStarted = true;
+                timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                time++;
+                                timeValue.setText(getTimerText());
+                            }
+                        });
+                    }
+                };
+                timer.scheduleAtFixedRate(timerTask, 0, 1000);
+            }
+            else{
+                timerStarted = false;
+                timerTask.cancel();
+            }
+    }
+
+    private String getTimerText() {
+        int rounded = (int) Math.round(time);
+
+        int seconds = ((rounded % 86400) % 3600) % 60;
+        int minutes = ((rounded % 86400) % 3600) / 60;
+        int hours = (rounded % 86400) / 3600;
+
+        return formatTime(seconds, minutes, hours);
+    }
+
+    private String formatTime(int seconds, int minutes, int hours) {
+        return String.format("%02d", hours) + " : " + String.format("%02d", minutes) + " : " + String.format("%02d", seconds);
     }
 
     private void onClickListeners() {
-        summaryButton.setOnClickListener(new View.OnClickListener() {
+        stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //add here whatever happens when you click the stop button
+                timeValue.setText(formatTime(0,0,0));
+
                 startActivity(summaryActivity.class);
             }
         });
 
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseButton.setVisibility(View.GONE);
+                stopButton.setVisibility(View.VISIBLE);
+                playButton.setVisibility(View.VISIBLE);
+                //add here whatever happens when you click the pause button
+                startTimer();
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopButton.setVisibility(View.GONE);
+                playButton.setVisibility(View.GONE);
+                pauseButton.setVisibility(View.VISIBLE);
+                //add here whatever happens when you click the play button
+                startTimer();
+            }
+        });
     }
 
     // Function to start a new activity. We pass in the destination Activity name as param
@@ -39,8 +114,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupUI() {
-        summaryButton = findViewById(R.id.summaryButtonID);
+        stopButton = findViewById(R.id.stopButtonID);
+        playButton = findViewById(R.id.playButtonID);
+        pauseButton = findViewById(R.id.pauseButtonId);
+        timeValue = findViewById(R.id.timeValueID);
+        timeText = findViewById(R.id.timeTextID);
+        distanceText = findViewById(R.id.distanceTextID);
+        distanceValue = findViewById(R.id.distanceValueID);
+        speedValue = findViewById(R.id.speedValueID);
+        speedText = findViewById(R.id.speedTextID);
+        BPMValue = findViewById(R.id.BPMValueID);
+        BPMText = findViewById(R.id.BPMTextID);
     }
-
 
 }
