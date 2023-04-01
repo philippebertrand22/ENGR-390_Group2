44e27739f2bf1d6e07bf4aa48d01b5779c3162bc
philippebertrand22 <90271955+philippebertrand22@users.noreply.github.com>
2023-04-01 13:08:29 -0400

    Saving loop before trying something else

--- UICode/app/src/main/java/com/example/ui_code/summaryActivity.java
+++ UICode/app/src/main/java/com/example/ui_code/summaryActivity.java
@@ -3,39 +3,21 @@ package com.example.ui_code;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -46,20 +28,13 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
-
-import android.os.Bundle;
 
 public class summaryActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap map;
+    GoogleMap pathMap;
     TextView text, text1;
+
+    Button  button;
 
     private DatabaseReference databaseLocationReference;
     private FirebaseAuth mAuth;
@@ -68,6 +43,8 @@ public class summaryActivity extends AppCompatActivity implements OnMapReadyCall
 
     private LatLng here;
     private double latitude, longitude;
+
+    private int entry_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +61,7 @@ public class summaryActivity extends AppCompatActivity implements OnMapReadyCall
     private void setupUI() {
         text = findViewById(R.id.textView);
         text1 = findViewById(R.id.textView1);
+        button = findViewById(R.id.button);
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser(); // Use this to get any user info from the database
@@ -92,30 +70,46 @@ public class summaryActivity extends AppCompatActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(@NonNull GoogleMap map) {
+        pathMap = map;
         findLatLng();
-
-        text1.setText(String.valueOf(longitude));
-
-        Polyline path = map.addPolyline(new PolylineOptions()
-                .add(
-                        new LatLng(latitude, longitude),
-                        new LatLng(38.4217937, -122.083922)));
-
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.4217937, -122.083922), 15));
     }
 
     private void findLatLng() {
         databaseLocationReference = FirebaseDatabase.getInstance().getReference("Users/" + userKey + "/Activities/Activity_" + MainActivity.latestNodeId);
-        
-        databaseLocationReference.child("1").get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+        PolylineOptions path = new PolylineOptions().width(8).color(Color.RED);
+
+        databaseLocationReference.child("entry_count").get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
             @Override
             public void onSuccess(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    String data_string = dataSnapshot.getValue().toString();
-                    latitude = Double.parseDouble(data_string.substring(data_string.indexOf('[') + 1, data_string.indexOf(']')));
-                    longitude = Double.parseDouble(data_string.substring(data_string.indexOf('{') + 1, data_string.indexOf('}')));
-                    text.setText(String.valueOf(longitude));
+                if (dataSnapshot.exists()){
+                    entry_count = Integer.parseInt(dataSnapshot.getValue().toString());
                 }
+            }
+        });
+
+ //       for(int n = 1; n <= entry_count; n++) {
+        int n = 1;
+            databaseLocationReference.child(String.valueOf(n)).get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                @Override
+                public void onSuccess(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        String data_string = dataSnapshot.getValue().toString();
+                        latitude = Double.parseDouble(data_string.substring(data_string.indexOf('[') + 1, data_string.indexOf(']')));
+                        longitude = Double.parseDouble(data_string.substring(data_string.indexOf('{') + 1, data_string.indexOf('}')));
+                        text.setText(String.valueOf(longitude));
+                        path.add(new LatLng(latitude, longitude));
+                    }
+                }
+            });
+ //       }
+        path.add(new LatLng(0, 0));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Polyline polyline = pathMap.addPolyline(path);
+                pathMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15));
+                Toast.makeText(view.getContext(), "ADDING PATH", Toast.LENGTH_SHORT).show();
             }
         });
     }
