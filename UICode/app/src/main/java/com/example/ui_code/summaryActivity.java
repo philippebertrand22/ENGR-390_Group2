012e7d7186fc4650e892074164e70cd536db5981
Gajanan Niranjanan <64200976+Gajanan-10@users.noreply.github.com>
2023-04-02 16:25:08 -0400

    Calories Burned calculated

--- UICode/app/src/main/java/com/example/ui_code/summaryActivity.java
+++ UICode/app/src/main/java/com/example/ui_code/summaryActivity.java
@@ -3,54 +3,27 @@ package com.example.ui_code;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import android.os.Bundle;
 
 public class summaryActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -74,7 +47,7 @@ public class summaryActivity extends AppCompatActivity implements OnMapReadyCall
 //            System.out.println(markers.get(0));
 //        }
     }
-
+//This is a test
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
@@ -107,4 +80,20 @@ public class summaryActivity extends AppCompatActivity implements OnMapReadyCall
                 return super.onOptionsItemSelected(item);
         }
     }
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("weight");
+
+    public void CaloriesBurned(DataSnapshot dataSnapshot){
+        int weight = dataSnapshot.child("weight").getValue(Integer.class);
+
+        double CaloriesBurned =  ((11.6 * 3.5 * Float.parseFloat(String.valueOf(weight))) / 200);
+
+        TextView resultTextView = findViewById(R.id.resultCalBurned);
+        resultTextView.setText(String.valueOf(CaloriesBurned));
+    }
+
 }
